4263ac1284a9b3118c34cfbcf79b38541fc1b445
Anthony Baumann <anthony.baumann@icloud.com>
2022-06-06 21:03:14 -0500

    added valid mountain array solution

---
+++ validMountainArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int length = arr.length;
+        if (length < 3) return false;
+
+        int i = 0;
+        while (i < length - 1 && arr[i] < arr[i+1]) {
+            i++;
+        }
+        
+        if (i == 0 || i == length - 1) return false;
+        
+        while (i < length - 1 && arr[i] > arr[i + 1]) {
+            i++;
+        }
+        return i == length - 1;
+    }
+}
