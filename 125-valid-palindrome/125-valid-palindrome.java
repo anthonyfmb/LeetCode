a4e60296c6547db9b3115e7e00e2bff53eea5138
Anthony Baumann <anthony.baumann@icloud.com>
2022-06-15 22:10:42 -0500

    Time: 1058 ms (8.41%), Space: 51.3 MB (12.21%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -2,9 +2,7 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "");
         s = s.toLowerCase();
-        System.out.println(s);
         int length = s.length();
-        //String beg = s.substring(0, length/2);
         int index = 0;
         for (int i = length - 1; i >= length/2; i--) {
             if (!s.substring(i, i+1).equals(s.substring(index, index+1))) {
