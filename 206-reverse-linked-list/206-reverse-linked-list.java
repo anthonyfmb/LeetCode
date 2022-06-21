c7a6a059ce6d4c2519a76d3d9eeeba7a4eb2cf48
Anthony Baumann <anthony.baumann@icloud.com>
2022-06-20 21:49:39 -0500

    Time: 0 ms (100.00%), Space: 43.3 MB (27.56%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -10,10 +10,9 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode next = null;
         ListNode prev = null;
         while (head != null) {
-            next = head.next;
+            ListNode next = head.next;
             head.next = prev;
             prev = head;
             head = next;
