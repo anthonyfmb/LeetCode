bda6fc84304c812884df84c7a1ac259e881efc5b
Anthony Baumann <anthony.baumann@icloud.com>
2022-06-23 12:39:01 -0500

    Time: 6 ms (55.39%), Space: 44.9 MB (62.52%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int[] pair = new int[2];
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
+                int[] pair = new int[2];
                 pair[0] = i;
                 pair[1] = map.get(nums[i]);
                 return pair;
@@ -11,6 +11,6 @@ class Solution {
             int opp = target - nums[i];
             map.put(opp, i);
         }
-        return pair;
+        return null;
     }
 }
