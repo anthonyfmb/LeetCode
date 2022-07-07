de17e871b1c19878737ca3e9e22196bfb9b0308f
Anthony Baumann <anthony.baumann@icloud.com>
2022-07-06 19:12:08 -0500

    Time: 63 ms (70.70%), Space: 61.8 MB (40.72%) - LeetHub

--- 346-moving-average-from-data-stream/346-moving-average-from-data-stream.java
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.java
@@ -1,24 +1,22 @@
 class MovingAverage {
     Queue<Integer> q;
     int length;
-    int total;
     double avg;
+
     public MovingAverage(int size) {
         q = new LinkedList<>();
         length = size;
-        total = 0;
         avg = 0;
     }
     
     public double next(int val) {
-        if (total >= length) {
+        if (q.size() == length) {
             q.add(val);
             avg -= q.remove();
             avg += val;
         } else {
             q.add(val);
             avg += val;
-            total++;
         }
         
         return avg / q.size();
